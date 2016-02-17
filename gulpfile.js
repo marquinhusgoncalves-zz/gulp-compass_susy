f35d4a15868871712417c939bb6d7fd00c8b81b7
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2016-02-17 17:38:38 -0200

    add imagemin, watch e notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,40 @@
 var gulp = require('gulp'),
-	compass = require('gulp-compass'),
-	rename = require('gulp-rename'),
-	autoprefix = require('gulp-autoprefixer'),
-  	nanocss = require('gulp-cssnano');
+    compass = require('gulp-compass'),
+    autoprefix = require('gulp-autoprefixer'),
+    nanocss = require('gulp-cssnano'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    notify = require("gulp-notify");
 
 gulp.task('styles', function() {
   gulp.src('./scss/*.scss')
-    .pipe(compass({
-      css: 'css',
-      sass: 'scss',
-      image: 'images',
-      require: ['susy', 'modular-scale'],
-      noCache: true,
-    }))
-    .pipe(autoprefix('last 2 version'))
-    .pipe(gulp.dest('./css'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(nanocss())
-    .pipe(gulp.dest('css'));
+  .pipe(compass({
+    css: 'css',
+    sass: 'scss',
+    image: 'images',
+    require: ['susy', 'modular-scale'],
+  }))
+  .pipe(autoprefix('last 2 version'))
+  .pipe(gulp.dest('./css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(nanocss())
+  .pipe(gulp.dest('css'))
+  .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('images/*')
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('img/'))
+    .pipe(notify({ message: 'Images task complete' }));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch('/**/*.scss', ['styles']);
+  gulp.watch('scss/*.scss', ['styles'])
+  gulp.watch('images/*', ['images']);
 });
 
-  gulp.task('default', ['styles']);
+gulp.task('default', function() {
+    gulp.start(['styles', 'images', 'watch']);
+});
