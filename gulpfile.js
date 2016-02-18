7b14da192bfd353bd29ff500eadddbf80eefee40
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2016-02-18 16:28:51 -0200

    gulp-jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp'),
+    jade = require('gulp-jade'),
     compass = require('gulp-compass'),
     autoprefix = require('gulp-autoprefixer'),
     nanocss = require('gulp-cssnano'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     notify = require("gulp-notify");
+
+gulp.task('jade', function () {
+    return gulp.src('jade/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(notify({ message: 'Jade task complete' }));
+});
 
 gulp.task('styles', function() {
   gulp.src('./scss/*.scss')
@@ -31,10 +41,11 @@ gulp.task('images', function() {
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
+  gulp.watch('jade/*.jade', ['jade'])
   gulp.watch('scss/*.scss', ['styles'])
   gulp.watch('images/*', ['images']);
 });
 
 gulp.task('default', function() {
-    gulp.start(['styles', 'images', 'watch']);
+    gulp.start(['jade', 'styles', 'images', 'watch']);
 });
